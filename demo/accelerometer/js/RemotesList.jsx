11a39e612829b4124efe7c3490264038060c6b80
Christophe Rosset <tophe@topheman.com>
2022-04-22 10:56:02 +0200

    feat(demo): send orientation data from remote to master

---
+++ demo/accelerometer/js/RemotesList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function RemotesList({ list }) {
+  if (list && list.length) {
+    return (
+      <ul>
+        {list.map(({ peerId, alpha, beta, gamma }) => (
+          <li key={peerId}>
+            <span>{peerId}</span>
+            <ul>
+              <li>alpha: {alpha}</li>
+              <li>beta: {beta}</li>
+              <li>gamma: {gamma}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return null;
+}
+
+RemotesList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.exact({
+      peerId: PropTypes.string,
+      alpha: PropTypes.number,
+      beta: PropTypes.number,
+      gamma: PropTypes.number,
+    })
+  ),
+};
