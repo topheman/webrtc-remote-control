ede741e321f58457982a13d456f04acf22cc7a51
Christophe Rosset <tophe@topheman.com>
2022-04-29 10:52:57 +0200

    feat(demo): rotate Phone3D with accelerometer - first draft

--- demo/accelerometer/js/Phone3D.jsx
+++ demo/accelerometer/js/Phone3D.jsx
@@ -1,14 +1,10 @@
 import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import PropTypes from "prop-types";
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef();
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame(() => {
-    ref.current.rotation.x += 0.01;
-  });
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
     <mesh {...props} ref={ref}>
@@ -18,14 +14,15 @@ function Box(props) {
   );
 }
 
-export default function Phone3D({ width, height }) {
+export default function Phone3D({ width, height, rotation }) {
+  console.log(rotation);
   return (
     <div style={{ position: "relative", width, height }}>
       <Canvas>
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
-        <Box position={[0, 0, 0]} />
+        <Box position={[0, 0, 0]} rotation={rotation} />
       </Canvas>
     </div>
   );
@@ -39,4 +36,5 @@ Phone3D.defaultProps = {
 Phone3D.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
+  rotation: PropTypes.arrayOf(PropTypes.number),
 };
